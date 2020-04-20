727a237b30f6663192dde9f197111081a0d65c63
Christophe Condomines <Chris.Condomines@gmail.com>
2020-04-20 17:26:46 +0200

    feat: Fill fields and format QuestionPage elements.

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -3,22 +3,46 @@ import { connect } from 'react-redux'
 
 class QuestionPage extends Component {
   render () {
-    const { id, question } = this.props
+    const { id, question, author } = this.props
     return (
       <div>
-        <h3>My QuestionPage</h3>
-        {id}
-        {question.optionOne.text}
+        <h4>{author.name} ask:</h4>
+              <div className="question">
+
+        <img src={author.avatarURL} alt={`avatar of ${author.name}`}
+          className="avatar" />
+          <div className="question-info">
+            <h3>Would you rather...</h3>
+            <form>
+              <input type="radio" name="votingOption" value="optionOne"/>
+              {question.optionOne.text}
+              <br/>
+              <input type="radio" name="votingOption" value="optionTwo"/>
+              {question.optionTwo.text}
+              <br/><input type="submit" value="submit"/>
+          <button
+              className='btn'
+              type='submit'
+              >
+            Submit
+          </button>            </form>
+          </div>
+          </div>
       </div>
     )
   }
 }
 
-function mapStateToProps ({ questions}, props) {
+function mapStateToProps ({ users, questions}, props) {
   const { id } = props.match.params
+  const question = questions[id]
+  const authorId = question.author
+  const author = users[authorId]
+
   return {
     id,
-    question: questions[id]
+    question,
+    author
   }
 }
 
