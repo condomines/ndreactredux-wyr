b6579ad2cdd9662ecaa6a698e0e7a2893bc0fda5
Christophe Condomines <Chris.Condomines@gmail.com>
2020-04-21 17:35:53 +0200

    feat: Split pending and done questions, and sort them.

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,38 +5,47 @@ import { connect } from 'react-redux'
 class Home extends Component {
 
   render () {
-    const questionsPending = this.props.questionsIds
-    const questionsDone = this.props.questionsIds
+    const { questionsPending, questionsDone } = this.props
 
     return (
       <div>
         <h3>Pending questions</h3>
         <ul>
-            {questionsPending.length > 0 && questionsPending.map( (questionId) => (
-          <li key={questionId}>
-              <Question id={questionId} />
+          {questionsPending.length > 0 && questionsPending.map( (question) => (
+          <li key={question[0]}>
+            <Question id={question[0]} />
           </li>
-
-              ))}
+          ))}
         </ul>
-        {/*
         <h3>Answered questions</h3>
         <ul>
-            {questionsDone.length > 0 && questionsDone.map( (question) => (
-          <li>
-              <Question id={question.id} />
+          {questionsDone.length > 0 && questionsDone.map( (question) => (
+          <li key={question[0]}>
+            <Question id={question[0]} />
           </li>
-              ))}
+          ))}
         </ul>
-      */}
       </div>
     )
   }
 }
 
-function mapStateToProp ({questions}) {
+function mapStateToProp ({questions, authedUser, users}) {
+  const sortedQuestions = Object.entries(questions)
+    .sort((a,b)=>questions[b[0]].timestamp - questions[a[0]].timestamp)
+
+  const questionsPending = sortedQuestions
+    .filter((elem) => (
+      !Object.keys(users[authedUser].answers).includes(elem[1].id)))
+
+  const questionsDone = sortedQuestions
+    .filter((elem) => (
+      Object.keys(users[authedUser].answers).includes(elem[1].id)))
+
   return {
-    questionsIds: Object.keys(questions)
+    questionsIds: Object.keys(questions),
+    questionsPending: questionsPending,
+    questionsDone: questionsDone
   }
 }
 
