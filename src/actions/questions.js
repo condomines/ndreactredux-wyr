a359306acece076985724c832dd7bd4b42ea4dfc
Christophe Condomines <Chris.Condomines@gmail.com>
2020-04-23 17:39:11 +0200

    Fix: Missing update from react-redux-loading to react-redux-loading-bar

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,4 @@
-import { showLoading, hideLoading } from 'react-redux-loading'
+import { showLoading, hideLoading } from 'react-redux-loading-bar'
 import { saveQuestionAnswer, saveQuestion } from '../utils/api.js'
 
 export const RECEIVE_DATA = 'RECEIVE_DATA'
