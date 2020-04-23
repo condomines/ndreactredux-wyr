812fccd072698fe7e2948665fcaf5d0860c0c46d
Christophe Condomines <Chris.Condomines@gmail.com>
2020-04-23 11:44:53 +0200

    Style: Format leaderboard.

--- src/components/User.js
+++ src/components/User.js
@@ -7,19 +7,25 @@ export default function User (props) {
   const score = totalQuestions + totalAnswers
 
   return (
-    <div className='container'>
-      <div className='avatar'>
+    <div className='container-user'>
+      <div className='container-avatar'>
         <img src={avatarURL} alt={`Avatar of ${name}`}
           className='avatar' />
       </div>
       <div className='user-info'>
-        {name}
-        <br/>Answered questions: {totalAnswers}
-        <br/>Created questions: {totalQuestions}
+        <div><b>{name}</b></div>
+        <div className='answer-line'>
+          <div>Answered questions:</div>
+          <div>{totalAnswers}</div>
+        </div>
+         <div className='answer-line'>
+          <div>Created questions:</div>
+          <div>{totalQuestions}</div>
+        </div>
       </div>
       <div className='score'>
-        Score
-        <br/>{score}
+        <div className='score-title'>Score</div>
+        <div className='score-value'>{score}</div>
       </div>
     </div>
     )
